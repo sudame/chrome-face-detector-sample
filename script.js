64582769ea3af21f617d3f81d6d269a49fb19380
sudame <su.glasses.923@gmail.com>
2018-03-21 10:19:17 +0900

    modify scope

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ document.addEventListener('load', () => {
     const faceDetector = new window.FaceDetector();
     const wrap = document.querySelector('.wrap');
     detect();
-    });
 
-async function detect(){
-    console.log('OK');
-    const img = document.querySelector('img');
-    const faces = await faceDetector.detect(img);
-    drawFaces(faces);
-}
+    async function detect(){
+        console.log('OK');
+        const img = document.querySelector('img');
+        const faces = await faceDetector.detect(img);
+        drawFaces(faces);
+    }
 
-async function drawFaces(faces){
-    faces.forEach(face => {
-        console.log(face);
-        const {width, height, top, left} = face.boundingBox;
+    async function drawFaces(faces){
+        faces.forEach(face => {
+            console.log(face);
+            const {width, height, top, left} = face.boundingBox;
 
-        // create the face
-        const faceBox = document.createElement('div');
-        faceBox.classList.add('face');
-        faceBox.style.cssText = `
-            width: ${width}px;
-            height: ${height}px;
-            top: ${top}px;
-            left: ${left}px;
-        `;
-
-        //create the eyes and mouth
-        face.landmarks.forEach(landmark => {
-            const el = document.createElement('div');
-            el.classList.add('landmark', landmark.type);
-            el.style.cssText = `
-                top: ${landmark.location.y - top};
-                left: ${landmark.location.x - left};
+            // create the face
+            const faceBox = document.createElement('div');
+            faceBox.classList.add('face');
+            faceBox.style.cssText = `
+                width: ${width}px;
+                height: ${height}px;
+                top: ${top}px;
+                left: ${left}px;
             `;
-            faceBox.appendChild(el);
-        });
-        wrap.appendChild(faceBox);
-        console.log(faceBox);
-    })
-}
+
+            //create the eyes and mouth
+            face.landmarks.forEach(landmark => {
+                const el = document.createElement('div');
+                el.classList.add('landmark', landmark.type);
+                el.style.cssText = `
+                    top: ${landmark.location.y - top};
+                    left: ${landmark.location.x - left};
+                `;
+                faceBox.appendChild(el);
+            });
+            wrap.appendChild(faceBox);
+            console.log(faceBox);
+        })
+    }
+});
