d73ed1e46ebd537487a9d5c9ea782303fe703ab7
Suda Mikihiro <s1711373@coins.tsukuba.ac.jp>
2018-03-21 10:25:25 +0900

    修正

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-document.addEventListener('load', () => {
-    const faceDetector = new window.FaceDetector();
-    const wrap = document.querySelector('.wrap');
-    detect();
+const faceDetector = new window.FaceDetector();
+const wrap = document.querySelector('.wrap');
+detect();
 
-    async function detect(){
-        console.log('OK');
-        const img = document.querySelector('img');
-        const faces = await faceDetector.detect(img);
-        drawFaces(faces);
-    }
+async function detect() {
+  console.log('OK');
+  const img = document.querySelector('img');
+  const faces = await faceDetector.detect(img);
+  drawFaces(faces);
+}
 
-    async function drawFaces(faces){
-        faces.forEach(face => {
-            console.log(face);
-            const {width, height, top, left} = face.boundingBox;
+async function drawFaces(faces) {
+  faces.forEach(face => {
+    console.log(face);
+    const { width, height, top, left } = face.boundingBox;
 
-            // create the face
-            const faceBox = document.createElement('div');
-            faceBox.classList.add('face');
-            faceBox.style.cssText = `
+    // create the face
+    const faceBox = document.createElement('div');
+    faceBox.classList.add('face');
+    faceBox.style.cssText = `
                 width: ${width}px;
                 height: ${height}px;
                 top: ${top}px;
                 left: ${left}px;
             `;
 
-            //create the eyes and mouth
-            face.landmarks.forEach(landmark => {
-                const el = document.createElement('div');
-                el.classList.add('landmark', landmark.type);
-                el.style.cssText = `
+    //create the eyes and mouth
+    face.landmarks.forEach(landmark => {
+      const el = document.createElement('div');
+      el.classList.add('landmark', landmark.type);
+      el.style.cssText = `
                     top: ${landmark.location.y - top};
                     left: ${landmark.location.x - left};
                 `;
-                faceBox.appendChild(el);
-            });
-            wrap.appendChild(faceBox);
-            console.log(faceBox);
-        })
-    }
-});
+      faceBox.appendChild(el);
+    });
+    wrap.appendChild(faceBox);
+    console.log(faceBox);
+  });
+}
